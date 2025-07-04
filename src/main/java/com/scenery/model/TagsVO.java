134ca887281675e5fa12644580c587ec2400c8fe
Potato Chip <tonychen504@gmail.com>
2025-07-04 19:59:04 +0800

    Updated Scenery related VO that has ManyToOne

--- src/main/java/com/scenery/model/TagsVO.java
+++ src/main/java/com/scenery/model/TagsVO.java
@@ -4,90 +4,69 @@ import java.sql.Timestamp;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tags")
-public class TagsVO implements java.io.Serializable{
+public class TagsVO implements java.io.Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer tagsId;
-	
+
 	@ManyToOne
-	@JoinColumn(name = "tagsdb_id")
-	private Integer tagsdbId;
-	
-	@OneToMany
-	@JoinColumn(name = "scenery_id")
-	private Integer sceneryId;
-	
-	
+	@JoinColumn(name = "tagsdb_id", nullable = false)
+	private TagsdbVO tagsdb;
+
+	@ManyToOne
+	@JoinColumn(name = "scenery_id", nullable = false)
+	private SceneryVO scenery;
+
 	@Column(updatable = false)
 	@CreationTimestamp
 	private Timestamp createTime;
-
 
 	public TagsVO() {
 		super();
 	}
 
-
-	public TagsVO(Integer tagsId, Integer tagsdbId, Integer sceneryId, Timestamp createTime) {
+	public TagsVO(Integer tagsId, TagsdbVO tagsdb, SceneryVO scenery, Timestamp createTime) {
 		super();
 		this.tagsId = tagsId;
-		this.tagsdbId = tagsdbId;
-		this.sceneryId = sceneryId;
+		this.tagsdb = tagsdb;
+		this.scenery = scenery;
 		this.createTime = createTime;
 	}
-
 
 	public Integer getTagsId() {
 		return tagsId;
 	}
 
-
 	public void setTagsId(Integer tagsId) {
 		this.tagsId = tagsId;
 	}
 
-
-	public Integer getTagsdbId() {
-		return tagsdbId;
+	public TagsdbVO getTagsdb() {
+		return tagsdb;
 	}
 
-
-	public void setTagsdbId(Integer tagsdbId) {
-		this.tagsdbId = tagsdbId;
+	public void setTagsdb(TagsdbVO tagsdb) {
+		this.tagsdb = tagsdb;
 	}
 
-
-	public Integer getSceneryId() {
-		return sceneryId;
+	public SceneryVO getScenery() {
+		return scenery;
 	}
 
-
-	public void setSceneryId(Integer sceneryId) {
-		this.sceneryId = sceneryId;
+	public void setScenery(SceneryVO scenery) {
+		this.scenery = scenery;
 	}
-
 
 	public Timestamp getCreateTime() {
 		return createTime;
 	}
 
-
 	public void setCreateTime(Timestamp createTime) {
 		this.createTime = createTime;
 	}
-	
-	
 }
