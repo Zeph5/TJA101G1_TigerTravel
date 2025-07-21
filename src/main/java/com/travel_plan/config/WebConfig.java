18f2f3cc65b18dc4eea4eadcda81077b1c237a79
Z.X.Tsai <classroom078087@gmail.com>
2025-07-21 12:53:16 +0800

    貼圖

---
+++ src/main/java/com/travel_plan/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.travel_plan.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	 public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	        // 對外開放 uploads 資料夾（放在專案根目錄）
+	        registry.addResourceHandler("/uploads/**")
+	                .addResourceLocations("file:/C:/TJA101-WebApp/spring%20boot/images/");
+	    }	
+
+
+}
